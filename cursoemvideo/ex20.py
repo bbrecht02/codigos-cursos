b17167dff4b55372831fed948fa7b9c1684f47c7
bbrecht02 <49658184+bbrecht02@users.noreply.github.com>
2019-04-15 17:18:43 -0300

    Delete ex20.py

--- cursoemvideo/ex20.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-aluno1= input("primeiro aluno:")
-aluno2= input("segundo aluno:")
-aluno3= input("terceiro aluno:")
-aluno4= input("quarto aluno:")
-lista= [aluno1,aluno2,aluno3,aluno4]
-sorteio= choice(lista)
-print(sorteio)
