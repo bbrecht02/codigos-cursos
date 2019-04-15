0c20ac0c85b7b663f24a5f27c5319b38e79572b4
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-04-15 17:58:52 -0300

    Delete ex012.py

--- cursoemvideo/ex012.py
+++
@@ -1,4 +0,0 @@
-valor= float(input("Qual o valor do produto? R$\n"))
-porc= valor*5/100
-newvalor= valor-porc
-print("O produto no valor de R${} ficará por R${} com 5% de desconto".format(valor,newvalor))
