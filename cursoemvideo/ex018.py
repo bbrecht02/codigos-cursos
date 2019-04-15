8317378dd7621d11f8cdb1bc8aac6650fbfd5c4f
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-04-15 18:00:00 -0300

    Delete ex018.py

--- cursoemvideo/ex018.py
+++
@@ -1,9 +0,0 @@
-from math import cos,sin,tan,radians,degrees,ceil
-angulo= float(input("Digite o angulo desejado:\n"))
-seno= sin(radians(angulo))
-print("O SENO de {}° vale {:.2f}".format(angulo,seno)))
-cosseno= cos(radians(angulo))
-print("O COSSENO de {}° vale {:.2f}".format(angulo,cosseno))
-tangente= tan(radians(angulo))
-print("A TANGENTE de {}° vale {:.2f}".format(angulo,tangente))
-
