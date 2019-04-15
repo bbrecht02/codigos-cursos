81f5496a70252a5c8a112c4de30be0c49b4965a7
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-04-15 17:59:27 -0300

    Delete ex015.py

--- cursoemvideo/ex015.py
+++
@@ -1,6 +0,0 @@
-dias= int(input("Quntos dias alugados?\n"))
-km= float(input("Quntos Km rodados?\n"))
-valor= (dias*60)+(km*0.15)
-print("O valor a ser pago é de R${}".format(valor))
-#R$60 por dia
-#R$0.15 por km
