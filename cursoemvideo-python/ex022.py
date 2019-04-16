0a2e26bb204d77ecee7e11a5bb2d7697879425ca
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-04-16 10:12:58 -0300

    Update ex022.py
    continuação do código

--- cursoemvideo-python/ex022.py
+++ cursoemvideo-python/ex022.py
@@ -3,6 +3,11 @@ nome= (str(input("digite seu nome completo:"))).strip()
 print ("Analisando seu nome...")
 time.sleep(1)
 print ("seu nome em letras maiúsculas é:", nome.upper())
-print ("seu nome em letras maiúsculas é:", nome.lower())
+print ("seu nome em letras minúsculas é:", nome.lower())
 print ("Seu nome tem ao todo {} letras".format(len(nome) - nome.count(" ")))
-print ("seu primeiro nome é {} e possui {} letras".format(nome[0:8],len(nome[0:8])))
+s= nome.split()
+print (s)
+print ("seu primeiro nome é: {} e ele tem {} letras".format(s[0],nome.find(' ')))
+
+
+
