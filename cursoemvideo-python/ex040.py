108dab8c312632f7796822138efb694aebcd6c4c
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-05-30 14:22:34 -0300

    Delete ex040.py

--- cursoemvideo-python/ex040.py
+++
@@ -1,8 +0,0 @@
-primeira_nota= float(input("Primeira nota: "))
-segunda_nota= float(input("Segunda nota: "))
-aprovado= float(7)
-recu= float(>=5 <=6.9)
-reprovado= float(>5)
-media= (primeira_nota + segunda_nota)/2
-if media==aprovado:
-     
